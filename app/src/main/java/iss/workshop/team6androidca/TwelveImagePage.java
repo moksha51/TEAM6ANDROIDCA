e423d0d57ab4d2e014f5752f476cdc68dddc41c2
Nina Lim <e0008256@u.nus.edu>
2022-07-10 21:20:22 +0800

    time&progbar layout

--- app/src/main/java/iss/workshop/team6androidca/TwelveImagePage.java
+++ app/src/main/java/iss/workshop/team6androidca/TwelveImagePage.java
@@ -3,17 +3,37 @@ package iss.workshop.team6androidca;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TwelveImagePage extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener  {
+
+    //Time count
+    TextView timeCount;
+    Button startReset;
+    Timer timer;
+    TimerTask timerTask;
+    Double time = 0.0;
+    boolean timerStarted = false; //this is the default
+
+    //Progress bar
+    TextView completionBar;
+
 
     private final int imagecount = 0;
     ActivityResultLauncher<Intent> rlSelectImage;
@@ -47,6 +67,16 @@ public class TwelveImagePage extends AppCompatActivity implements AdapterView.On
             gridView.setAdapter(adapter);
             gridView.setOnItemClickListener(this);
         }
+
+
+        //Time Count
+        timeCount = findViewById(R.id.timeCount);
+        startReset = findViewById(R.id.startReset);
+        timer = new Timer();
+
+        //Progress Bar
+        completionBar = findViewById(R.id.xOutOf6ImageSelectCounter);
+        completionBar.setMax(6);
     }
 
     protected void registerForResult(){
@@ -94,4 +124,90 @@ public class TwelveImagePage extends AppCompatActivity implements AdapterView.On
             finish();
         }
     }
+
+
+
+    // ProgressBar and Timer
+    public void resetTapped (View v){
+
+        AlertDialog.Builder resetAlert = new AlertDialog.Builder(this);
+        resetAlert.setTitle("Restart game");
+        resetAlert.setMessage("Are you sure you want to restart the game?");
+        resetAlert.setPositiveButton("Restart", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (timerTask != null) // got some task
+                {
+                    timerTask.cancel();
+                    time = 0.0;
+                    timerStarted=false;
+                    timeCount.setText(formatTime(0,0,0));
+
+                    newProgress == 0;
+                }
+            }
+        });
+
+        resetAlert.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+    }
+
+    public void startGame (View v){
+
+        if (timerStarted == false) // if timer is not started
+        {
+            timerStarted = true; //start timer
+            startReset.setText("RESTART"); // change button to stop because timer started
+            startReset.setTextColor(ContextCompat.getColor(this, teal_200));
+
+            startTimer();
+        }
+        else{ //if timer is started
+            timerStarted = false; //stop timer
+            startReset.setText("START");
+
+            timerTask.cancel();
+
+        }
+    }
+
+    private void startTimer(){
+        timerTask = new TimerTask()
+        {
+            @Override
+            public void run(){
+                runOnUiThread(new Runnable(){
+                    @Override
+                    public void run(){
+                        time++;
+                        timeCount.setText(getTimerText());
+                    }
+                });
+
+            }
+        };
+
+        timer.scheduleAtFixedRate(timerTask,0,1000);
+    }
+
+    private String getTimerText()
+    {
+        int rounded = (int) Math.round(time);
+        int seconds = ((rounded % 86400) % 3600) % 60;
+        int minutes = ((rounded % 86400) % 3600) / 60;
+        int hours = ((rounded % 86400) / 3600);
+
+        return formatTime(seconds, minutes, hours);
+
+    }
+
+    private String formatTime(int seconds, int minutes, int hours){
+        return String.format("%2d", hours) + ":" + String.format("%2d", minutes) + ":" + String.format("%2d", seconds);
+    }
+
+
 }
